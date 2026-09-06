95bb8783f588349611de8277536e5a849887dec3
Lazar Vasiljevic <lazar2703@gmail.com>
2026-09-07 00:01:07 +0200

    Dodat user role

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
+            'role' => 'user',
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
